839a7ac0ccd8e72872c38ba9ada7fe1d7817ef85
DesignSupply <ogawa@designsupply-web.com>
2020-04-23 03:22:02 +0900

    [add] TypeScriptのソースファイルにレガシーブラウザチェック用の関数を追加

--- develop/src/ts/index.ts
+++ develop/src/ts/index.ts
@@ -3,8 +3,31 @@
 import $ from 'jquery';
 import moduleFunction from './modules/module';
 
-console.log(moduleFunction('Hello World!!'));
+console.log(moduleFunction('Hello World!'));
 
+// legacy browser alert
+const isLegacyIE = (): void | boolean => {
+  const targetUserAgent: string = navigator.userAgent,
+    notice: string = 'ご利用のウェブブラウザでは当サイトを正常に閲覧できません。ブラウザのバージョンを最新にして再度アクセスしてください。',
+    targets: Array<string> = [ 
+      'MSIE 6.0', // Internet Explorer 6
+      'MSIE 7.0', // Internet Explorer 7
+      'MSIE 8.0', // Internet Explorer 8
+      'MSIE 9.0', // Internet Explorer 9
+      'MSIE 10.0', // Internet Explorer 10
+      'Trident/7.0' // Internet Explorer 11
+    ];
+    targets.forEach((target: string): void | boolean => {
+    if(targetUserAgent.indexOf(target) != -1) {
+      alert(notice);
+    } else {
+      return false;
+    }
+  });
+};
+isLegacyIE();
+
+// jQuery
 $(function(){
   console.log('jQuery is ready.');
 });
