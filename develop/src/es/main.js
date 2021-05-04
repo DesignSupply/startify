b3bd3243856b888726f9ee52173bbf27fd6d7b87
DesignSupply <ogawa@designsupply-web.com>
2021-05-04 10:37:45 +0900

    [add] ブラウザ・OSユーザーエージェント判別追加

--- develop/src/es/main.js
+++ develop/src/es/main.js
@@ -3,17 +3,17 @@
 const $ = require('jQuery');
 const Vue = require('vue');
 
-// legacy IE browser alert
+// checking legacy IE
 const isLegacyIE = () => {
-  const currentUserAgent = navigator.userAgent,
+  const currentUserAgent = window.navigator.userAgent.toLowerCase(),
     notice = 'ご利用のウェブブラウザでは当サイトを正常に閲覧できません。ブラウザのバージョンを最新にして再度アクセスしてください。',
     targets = [ 
-      'MSIE 6.0', // Internet Explorer 6
-      'MSIE 7.0', // Internet Explorer 7
-      'MSIE 8.0', // Internet Explorer 8
-      'MSIE 9.0', // Internet Explorer 9
-      'MSIE 10.0', // Internet Explorer 10
-      'Trident/7.0' // Internet Explorer 11
+      'msie 6.0', // Internet Explorer 6
+      'msie 7.0', // Internet Explorer 7
+      'msie 8.0', // Internet Explorer 8
+      'msie 9.0', // Internet Explorer 9
+      'msie 10.0', // Internet Explorer 10
+      'trident/7.0' // Internet Explorer 11
     ];
     Array.prototype.slice.call(targets).forEach((target) => {
     if(currentUserAgent.indexOf(target) !== -1) {
@@ -24,6 +24,50 @@ const isLegacyIE = () => {
   });
 };
 isLegacyIE();
+
+// checking OS
+const findOS = () => {
+  const currentUserAgent = window.navigator.userAgent.toLowerCase();
+  if(currentUserAgent.indexOf('windows nt') !== -1) {
+    return 'Windows';
+  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') !== -1)) {
+    return 'Android Mobile';
+  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') === -1)) {
+    return 'Android Tablet';
+  } else if(currentUserAgent.indexOf('iphone') !== -1) {
+    return 'iOS iPhone';
+  } else if(currentUserAgent.indexOf('ipad') !== -1) {
+    return 'iOS iPad';
+  } else if(currentUserAgent.indexOf('mac os') !== -1) {
+    return 'Mac OS';
+  } else {
+    return 'other OS';
+  }
+};
+findOS();
+
+// checking browser
+const findBrowser = () => {
+  const currentUserAgent = window.navigator.userAgent.toLowerCase();
+  if(currentUserAgent.indexOf('msie') !== -1 || currentUserAgent.indexOf('trident') !== -1) {
+    return 'Internet Explorer';
+  } else if(currentUserAgent.indexOf('edge') !== -1) {
+    return 'Microsoft Edge';
+  } else if(currentUserAgent.indexOf('edg') !== -1) {
+    return 'Chromium Edge';
+  } else if(currentUserAgent.indexOf('opr') !== -1) {
+    return 'Opera';
+  } else if(currentUserAgent.indexOf('chrome') !== -1) {
+    return 'Google Chrome';
+  } else if(currentUserAgent.indexOf('safari') !== -1) {
+    return 'Safari';
+  } else if(currentUserAgent.indexOf('firefox') !== -1) {
+    return 'FireFox';
+  } else {
+    return 'other Browser';
+  }
+};
+findBrowser();
 
 // jQuery
 $(function() {
--- develop/src/ts/index.ts
+++ develop/src/ts/index.ts
@@ -6,17 +6,17 @@ import moduleFunction from './modules/module';
 
 console.log(moduleFunction('Hello World!'));
 
-// legacy IE browser alert
+// checking legacy IE
 const isLegacyIE = (): void | boolean => {
-  const currentUserAgent: string = navigator.userAgent,
+  const currentUserAgent: string = window.navigator.userAgent.toLowerCase(),
     notice: string = 'ご利用のウェブブラウザでは当サイトを正常に閲覧できません。ブラウザのバージョンを最新にして再度アクセスしてください。',
     targets: Array<string> = [ 
-      'MSIE 6.0', // Internet Explorer 6
-      'MSIE 7.0', // Internet Explorer 7
-      'MSIE 8.0', // Internet Explorer 8
-      'MSIE 9.0', // Internet Explorer 9
-      'MSIE 10.0', // Internet Explorer 10
-      'Trident/7.0' // Internet Explorer 11
+      'msie 6.0', // Internet Explorer 6
+      'msie 7.0', // Internet Explorer 7
+      'msie 8.0', // Internet Explorer 8
+      'msie 9.0', // Internet Explorer 9
+      'msie 10.0', // Internet Explorer 10
+      'trident/7.0' // Internet Explorer 11
     ];
     Array.prototype.slice.call(targets).forEach((target: string): void | boolean => {
     if(currentUserAgent.indexOf(target) !== -1) {
@@ -27,6 +27,50 @@ const isLegacyIE = (): void | boolean => {
   });
 };
 isLegacyIE();
+
+// checking OS
+const findOS = (): string => {
+  const currentUserAgent: string = window.navigator.userAgent.toLowerCase();
+  if(currentUserAgent.indexOf('windows nt') !== -1) {
+    return 'Windows';
+  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') !== -1)) {
+    return 'Android Mobile';
+  } else if((currentUserAgent.indexOf('android') !== -1) && (currentUserAgent.indexOf('mobile') === -1)) {
+    return 'Android Tablet';
+  } else if(currentUserAgent.indexOf('iphone') !== -1) {
+    return 'iOS iPhone';
+  } else if(currentUserAgent.indexOf('ipad') !== -1) {
+    return 'iOS iPad';
+  } else if(currentUserAgent.indexOf('mac os') !== -1) {
+    return 'Mac OS';
+  } else {
+    return 'other OS';
+  }
+};
+findOS();
+
+// checking browser
+const findBrowser = (): string => {
+  const currentUserAgent: string = window.navigator.userAgent.toLowerCase();
+  if(currentUserAgent.indexOf('msie') !== -1 || currentUserAgent.indexOf('trident') !== -1) {
+    return 'Internet Explorer';
+  } else if(currentUserAgent.indexOf('edge') !== -1) {
+    return 'Microsoft Edge';
+  } else if(currentUserAgent.indexOf('edg') !== -1) {
+    return 'Chromium Edge';
+  } else if(currentUserAgent.indexOf('opr') !== -1) {
+    return 'Opera';
+  } else if(currentUserAgent.indexOf('chrome') !== -1) {
+    return 'Google Chrome';
+  } else if(currentUserAgent.indexOf('safari') !== -1) {
+    return 'Safari';
+  } else if(currentUserAgent.indexOf('firefox') !== -1) {
+    return 'FireFox';
+  } else {
+    return 'other Browser';
+  }
+};
+findBrowser();
 
 // jQuery
 $(function() {
